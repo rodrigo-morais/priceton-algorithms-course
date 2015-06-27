09abde1c2843b2bbe3b73f3ed2e17efb3fe8bbfa
Rodrigo Morais <morais.rm@gmail.com>
2015-06-27 07:07:10 -0300

    Week 1-1.

---
+++ Week 1-1/quickFind/quickFind/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quickFind
+{
+    class Program
+    {
+        public class QuickFindUF
+        {
+            public int[] id;
+
+            public QuickFindUF(int N)
+            {
+                id = new int[N];
+                for (int i = 0; i < N; i++)
+                {
+                    id[i] = i;
+                }
+            }
+
+            public bool connected(int p, int q)
+            {
+                return id[p] == id[q];
+            }
+
+            public void union(int p, int q)
+            {
+                int pid = id[p];
+                int qid = id[q];
+                for (int i = 0; i < id.Count(); i++)
+                {
+                    if (id[i] == pid)
+                    {
+                        id[i] = qid;
+                    }
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            var qu = new QuickFindUF(10);
+
+            var line = Console.ReadLine();
+            foreach (var par in line.Split(' '))
+            {
+                qu.union(int.Parse(par.Split('-')[0]), int.Parse(par.Split('-')[1]));
+            }
+
+            foreach (var _id in qu.id)
+            {
+                Console.Write(_id);
+                Console.Write(" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week 1-1/quickUnion/quickUnion/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quickUnion
+{
+    class Program
+    {
+        public class QuickFindUF
+        {
+            public int[] id;
+            public int[] sz;
+
+            public QuickFindUF(int N)
+            {
+                id = new int[N];
+                sz = new int[N];
+                for (int i = 0; i < N; i++)
+                {
+                    id[i] = i;
+                    sz[i] = 1;
+                }
+            }
+
+            private int root(int i)
+            {
+                while (i != id[i])
+                {
+                    i = id[i];
+                }
+                return i;
+            }
+
+            public bool connected(int p, int q)
+            {
+                return root(p) == root(q);
+            }
+
+            public void union(int p, int q)
+            {
+                int i = root(p);
+                int j = root(q);
+                if (sz[i] < sz[j])
+                {
+                    id[i] = j;
+                    sz[j] += sz[i];
+                }
+                else
+                {
+                    id[j] = i;
+                    sz[i] += sz[j];
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            var qu = new QuickFindUF(10);
+
+            var line = Console.ReadLine();
+            foreach (var par in line.Split(' '))
+            {
+                qu.union(int.Parse(par.Split('-')[0]), int.Parse(par.Split('-')[1]));
+            }
+
+            foreach (var _id in qu.id)
+            {
+                Console.Write(_id);
+                Console.Write(" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
